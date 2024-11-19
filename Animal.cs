e2ff73291e8fd08371c9caf1cb7f0899fb991281
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-19 21:48:18 +0500

    .

---
+++ Animal.cs
@@ -0,0 +1,24 @@
+namespace Animal;
+
+public class Animal
+{
+    public string DomesticationStatus  { get; set; }
+    
+    public string Species { get; set; }
+    
+    public string DietType { get; set; }
+    
+    public int Age { get; set; }
+    
+    public string Gender { get; set; }
+    
+    public int Weight { get; set; }
+    
+    public int Height { get; set; }
+     
+    public string Color { get; set; }
+    
+    public string habitat { get; set; }
+    
+    
+}
