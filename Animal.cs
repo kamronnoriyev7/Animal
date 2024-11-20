75a94999a61486c8b6e16a556e9400442d681d6e
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-20 11:07:40 +0500

    .

--- Animal.cs
+++ Animal.cs
@@ -2,23 +2,31 @@ namespace Animal;
 
 public class Animal
 {
-    public string DomesticationStatus  { get; set; }
-    
+    public string Nickname { get; set; }
+
     public string Species { get; set; }
-    
+
     public string DietType { get; set; }
-    
+
     public int Age { get; set; }
-    
+
     public string Gender { get; set; }
-    
+
     public int Weight { get; set; }
-    
+
     public int Height { get; set; }
-     
+
     public string Color { get; set; }
-    
-    public string habitat { get; set; }
-    
-    
+
+    public string Habitat { get; set; }
+
+    public void AnimalNickname()
+    {
+        Console.WriteLine($"Hayvon laqabi: {Nickname} ");
+    }
+
+    public void AnimalSpecies()
+    {
+        Console.WriteLine($"Hayvon turi:  {Species} ");
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿using Animal;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal.Animal mushuk = new Animal.Animal()
+        {
+            Nickname = "Najim",
+            Species = "Uy hayvoni",
+            DietType = "Milk",
+            Gender = "Male",
+            Age = 2,
+            Weight = 2,
+            Height = 20,
+            Color = "Narda gray",
+            Habitat = "home"
+        };
+        mushuk.AnimalNickname();
+        mushuk.AnimalSpecies();
+        Console.WriteLine();
+        Animal.Animal Lion = new Animal.Animal()
+        {
+            Nickname = "Lion",
+            Species = "Yovvoyi hayvon",
+            DietType = "Meat",
+            Gender = "Male",
+            Age = 5,
+            Weight = 120,
+            Height = 130,
+            Color = "Golden yellow",
+            Habitat = "Savanna"
+        };
+        Lion.AnimalNickname();
+        Lion.AnimalSpecies();
+        Console.WriteLine();
+        Animal.Animal monkey = new Animal.Animal()
+        {
+            Nickname = "Monkey",
+            Species = "Yovvoyi hayvon",
+            DietType = "fruit",
+            Gender = "female",
+            Age = 3,
+            Weight = 50,
+            Height = 150,
+            Color = "Brown",
+            Habitat = "Jungle"
+        };
+        monkey.AnimalNickname();
+        monkey.AnimalSpecies();
+    }
+}
